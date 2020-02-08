6b5c70b152983b9ba5a9fdbaa5b8e17099a29d88
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-02-08 11:44:44 +0100

    added CustomizationFragment layout

--- app/src/main/java/com/arainko/nawts/fragments/CustomizationFragment.kt
+++ app/src/main/java/com/arainko/nawts/fragments/CustomizationFragment.kt
@@ -6,19 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arainko.nawts.R
+import com.arainko.nawts.model.NoteViewModel
+import com.arainko.nawts.persistence.Note
+import com.arainko.nawts.view.NoteAdapter
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CustomizationFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customization, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val model: NoteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        val adapter = NoteAdapter(model)
+        model.notes.observe(this, Observer<List<Note>> { adapter.submitList(it.reversed()) })
+
+        return inflater.inflate(R.layout.fragment_customization, container, false).apply {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            recyclerView.adapter = adapter
+            recyclerView.setHasFixedSize(true)
+        }
     }
 
 
--- app/src/main/java/com/arainko/nawts/fragments/MainFragment.kt
+++ app/src/main/java/com/arainko/nawts/fragments/MainFragment.kt
@@ -4,8 +4,10 @@ package com.arainko.nawts.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -22,7 +24,7 @@ import kotlinx.android.synthetic.main.fragment_main.view.*
 /**
  * A simple [Fragment] subclass.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), Toolbar.OnMenuItemClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val model: NoteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
         val adapter = NoteAdapter(model)
@@ -36,8 +38,16 @@ class MainFragment : Fragment() {
                 val action = MainFragmentDirections.actionMainFragmentToNoteEditFragment("", "")
                 findNavController().navigate(action)
             }
-            bottom_app_bar.set
+            bottom_app_bar.setOnMenuItemClickListener(this@MainFragment)
         }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.navCustomization -> {
+            findNavController().navigate(R.id.action_mainFragment_to_customizationFragment)
+            true
+        }
+        else -> false
     }
 
 }
