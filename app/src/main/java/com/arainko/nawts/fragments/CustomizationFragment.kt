59d0ee021d9447bbc5c361e3d37d1fa3355d9d0c
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-02-11 23:44:58 +0100

    laying the groundwork for CustomizationFragment removal (to be replaced by a StateManager inside MainFragment) added color picker for customization

--- app/src/main/java/com/arainko/nawts/fragments/CustomizationFragment.kt
+++ app/src/main/java/com/arainko/nawts/fragments/CustomizationFragment.kt
@@ -1,22 +1,30 @@
 package com.arainko.nawts.fragments
 
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.children
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arainko.nawts.R
+import com.arainko.nawts.extensions.asColor
 import com.arainko.nawts.model.NoteViewModel
 import com.arainko.nawts.persistence.Note
 import com.arainko.nawts.view.HolderBehavior
 import com.arainko.nawts.view.NoteAdapter
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.bottom_sheet_customization.*
+import kotlinx.android.synthetic.main.bottom_sheet_customization.view.*
 import kotlinx.android.synthetic.main.fragment_customization.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
@@ -43,7 +51,25 @@ class CustomizationFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-        model.notes.observe(this, Observer<List<Note>> { adapter.submitList(it.reversed()) })
+        model.notes.observe(this, Observer { adapter.submitList(it.reversed()) })
+
+        val colors = listOf("#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
+            "#f205e6" ,"#1c0365" ,"#14a9ad")
+
+        repeat(colors.size) {
+            val button = layoutInflater.inflate(
+                R.layout.color_button_layout,
+                scrollContainer,
+                false
+            ) as Button
+            button.setBackgroundColor(colors[it].asColor())
+            scrollContainer.addView(button)
+        }
+
+        val children = scrollContainer
+
+        Log.d("CHILDREN BUTTONS", "${children}")
+
     }
 
     override fun onHolderClick(holderItem: Note, view: View) {
