9df1cf182824a4c91def3115072620506d477709
arainko <aleksander.rainko99@gmail.com>
2020-02-19 22:33:23 +0100

    added temporal note color reset

--- app/src/main/java/com/arainko/nawts/fragments/BottomSheetCustomizerFragment.kt
+++ app/src/main/java/com/arainko/nawts/fragments/BottomSheetCustomizerFragment.kt
@@ -1,11 +1,12 @@
 package com.arainko.nawts.fragments
 
 import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.viewModels
 import com.arainko.nawts.R
 import com.arainko.nawts.extensions.addTo
@@ -21,11 +22,15 @@ import kotlinx.android.synthetic.main.bottom_sheet_customization.*
 import kotlinx.android.synthetic.main.note_layout.view.*
 import kotlin.properties.Delegates
 
-class BottomSheetCustomizerFragment() : BottomSheetDialogFragment() {
+class BottomSheetCustomizerFragment() : BottomSheetDialogFragment(), PopupMenu.OnMenuItemClickListener {
 
+    private val model: NoteViewModel by viewModels()
     private lateinit var note: Note
     private lateinit var adapter: NoteAdapter
     private var position: Int by Delegates.notNull()
+    private lateinit var currentBackgroundColor: String
+    private lateinit var currentStrokeColor: String
+    private lateinit var colorToButtonMap: Map<String, MaterialButton>
 
     constructor(note: Note, adapter: NoteAdapter, position: Int) : this() {
         this.note = note
@@ -33,10 +38,10 @@ class BottomSheetCustomizerFragment() : BottomSheetDialogFragment() {
         this.position = position
     }
 
-    private val model: NoteViewModel by viewModels()
-    private lateinit var currentBackgroundColor: String
-    private lateinit var currentStrokeColor: String
-    private lateinit var colorToButtonMap: Map<String, MaterialButton>
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (!this::note.isInitialized) dismissAllowingStateLoss()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,24 +49,26 @@ class BottomSheetCustomizerFragment() : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.bottom_sheet_customization, container, false)
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (!this::note.isInitialized) dismissAllowingStateLoss()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
             currentBackgroundColor = note.style.backgroundColor
             currentStrokeColor = note.style.strokeColor
             val previewHeader = note.header.removeTrailingLines()
             val previewContent = note.content.removeTrailingLines()
 
-            cardPreview.apply {
+            cardPreview.run {
                 cardHeader.text = previewHeader
                 cardText.text = previewContent
                 (this as MaterialCardView).setCardBackgroundColor(currentBackgroundColor.asIntColor())
+                setOnLongClickListener {
+                    PopupMenu(this.context, this).apply {
+                        setOnMenuItemClickListener(this@BottomSheetCustomizerFragment)
+                        inflate(R.menu.customization_menu)
+                        show()
+                    }
+                    true
+                }
             }
 
             colorToButtonMap = resources.getStringArray(R.array.colors).map {
@@ -74,7 +81,7 @@ class BottomSheetCustomizerFragment() : BottomSheetDialogFragment() {
                     addTo(scrollContainer)
                     setBackgroundColor(hexColor.asIntColor())
                     setOnClickListener {
-                        updateButtonIcons(currentBackgroundColor, hexColor)
+                        updateButtonStrokes(currentBackgroundColor, hexColor)
                         (cardPreview as MaterialCardView).setCardBackgroundColor(hexColor.asIntColor())
                         currentBackgroundColor = hexColor
                     }
@@ -87,16 +94,33 @@ class BottomSheetCustomizerFragment() : BottomSheetDialogFragment() {
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onPause() {
+        super.onPause()
             note.style.backgroundColor = currentBackgroundColor
             adapter.notifyItemChanged(position)
             model.updateNote(note)
     }
 
-    private fun updateButtonIcons(oldColor: String, newColor: String) {
+    private fun updateButtonStrokes(oldColor: String, newColor: String) {
         colorToButtonMap[oldColor]?.strokeWidth = 0
         colorToButtonMap[newColor]?.strokeWidth = 10
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.defaultBackgroundColor -> {
+            currentBackgroundColor = "#ffffff"
+            (cardPreview as MaterialCardView).run {
+                setCardBackgroundColor(currentBackgroundColor.asIntColor())
+            }
+            true
+        }
+        else -> {
+            currentStrokeColor = "#00000000"
+            (cardPreview as MaterialCardView).run {
+                strokeColor = currentStrokeColor.asIntColor()
+            }
+            true
+        }
     }
 
 }
